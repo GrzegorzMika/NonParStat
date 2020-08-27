f33797bda65a85a725163c1b1a9bbd55ffbd4cbb
Grzegorz <g.w.mika@gmail.com>
2020-08-27 16:32:14 +0200

    Cucconi test II

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -1,8 +1,51 @@
 import unittest
 
-class MyTestCase(unittest.TestCase):
-    def test_something(self):
-        self.assertEqual(True, False)
+from nonparstat.cucconi import *
+
+
+class Cucconi(unittest.TestCase):
+    def test_equal(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertGreater(cucconi_test(sample_a, sample_b).pvalue, 0.99)
+
+    def test_mean(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=1, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b).pvalue, 0.001)
+
+    def test_variance(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=0, scale=10, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b).pvalue, 0.001)
+
+    def test_mean_variance(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=10, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b).pvalue, 0.001)
+
+    def test_equal_permutations(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertGreater(cucconi_test(sample_a, sample_b, method='permutation').pvalue, 0.99)
+
+    def test_mean_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=1, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b, method='permutation').pvalue, 0.001)
+
+    def test_variance_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=0, scale=10, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b, method='permutation').pvalue, 0.001)
+
+    def test_mean_variance_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=10, size=100)
+        self.assertLess(cucconi_test(sample_a, sample_b, method='permutation').pvalue, 0.001)
+
+    def test_method(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertRaises(ValueError, cucconi_test, sample_a, sample_b, method='exact')
+
 
 if __name__ == '__main__':
     unittest.main()
