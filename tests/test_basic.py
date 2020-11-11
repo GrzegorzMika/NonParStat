dd586cb17d9748b70b0e135798904d58bc8e997d
Grzegorz <g.w.mika@gmail.com>
2020-11-11 21:06:49 +0100

    add tests for multisample Cucconi test

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -48,6 +48,50 @@ class Cucconi(unittest.TestCase):
         self.assertRaises(ValueError, cucconi_test, sample_a, sample_b, method='exact')
 
 
+class Cucconi_multisample(unittest.TestCase):
+    def test_equal(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertGreater(cucconi_multisample_test([sample_a, sample_b]).pvalue, 0.99)
+
+    def test_mean(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=1, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b]).pvalue, 0.001)
+
+    def test_variance(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=0, scale=10, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b]).pvalue, 0.001)
+
+    def test_mean_variance(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=10, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b]).pvalue, 0.001)
+
+    def test_equal_permutations(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertGreater(cucconi_multisample_test([sample_a, sample_b], method='permutation').pvalue, 0.99)
+
+    def test_mean_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=1, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b], method='permutation').pvalue, 0.001)
+
+    def test_variance_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=0, scale=10, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b], method='permutation').pvalue, 0.001)
+
+    def test_mean_variance_permutations(self):
+        sample_a = np.random.normal(loc=0, scale=1, size=100)
+        sample_b = np.random.normal(loc=10, scale=10, size=100)
+        self.assertLess(cucconi_multisample_test([sample_a, sample_b], method='permutation').pvalue, 0.001)
+
+    def test_method(self):
+        sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
+        self.assertRaises(ValueError, cucconi_multisample_test, [sample_a, sample_b], method='exact')
+
+
 class PodgorGastwirth(unittest.TestCase):
     def test_equal(self):
         sample_a = sample_b = np.random.normal(loc=0, scale=1, size=100)
